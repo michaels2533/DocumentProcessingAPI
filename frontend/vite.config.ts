aa695a683ae904ff511beedcb982cc759ee111c0
michaels2533 <michael.stawowski@wsu.edu>
2026-05-25 20:44:44 -0700

    Configured Vite proxy config to load env variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://backend:8000",
-        changeOrigin: true,
+export default defineConfig(({mode}) => {
+
+  //Loads .env file based on 'mode' in current working directory
+  const env = loadEnv(mode, process.cwd())
+
+  // Vite Config
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  }
+  });
